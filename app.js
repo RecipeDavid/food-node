949559b3e4a4384f66d80266e21b8af5a4569430
Dan.Garfield <DanGarfield@tuwig-10122-MBP-Dan-Garfield.local>
2017-01-11 15:45:37 +0000

    example change

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // render the error page change
   res.status(err.status || 500);
   res.render('error');
 });
